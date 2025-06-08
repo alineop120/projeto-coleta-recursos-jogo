fa54612de6d8ac117fa2f4cb847f1a629c6e81c4
Aline Pinho <alineveira2@gmail.com>
2025-06-08 17:29:36 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,14 @@ from flask_cors import CORS
 from routes.player import player_routes, obter_estado_e_lock
 from routes.npc import npc_routes, configurar_npcs, NPCManager
 
+from services import adicionar_recurso_periodicamente
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Registra as blueprints
+app.register_blueprint(player_routes, url_prefix='/player')
+app.register_blueprint(npc_routes, url_prefix='/npc')
 
 # Obter estado e lock do módulo do jogador
 estado_jogo, campo_lock = obter_estado_e_lock()
@@ -17,9 +23,8 @@ configurar_npcs(estado_jogo)
 npc_manager = NPCManager(estado_jogo, campo_lock)
 npc_manager.start()
 
-# Registrar rotas
-app.register_blueprint(player_routes)
-app.register_blueprint(npc_routes)
+# supondo que estado_jogo já esteja criado
+adicionar_recurso_periodicamente(estado_jogo)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
