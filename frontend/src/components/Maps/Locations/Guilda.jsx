cb032b5e03481dc99cd0ee1f005fd8fa8522d342
Aline Pinho <alineveira2@gmail.com>
2025-06-08 16:26:46 -0300

    Create Guilda.jsx

---
+++ frontend/src/components/Maps/Locations/Guilda.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Guilda() {
+    return (
+        <div style={{ border: '1px solid #aaa', padding: 10, marginTop: 10, borderRadius: 6 }}>
+            <h3>Guilda</h3>
+            <p>Bem-vindo à Guilda! Aqui você pode interagir com NPCs, receber missões, etc.</p>
+            {/* Pode expandir com mais funcionalidades depois */}
+        </div>
+    );
+}
+
+export default Guilda;
