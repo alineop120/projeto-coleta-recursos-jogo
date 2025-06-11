f082de08d04c6100842a83c4c08aff86b5829859
Aline Pinho <alineveira2@gmail.com>
2025-06-10 21:36:57 -0300

    feat: apagar os arquivos da pasta do backend

--- backend/models/resources.py
+++
@@ -1,6 +0,0 @@
-recursos_no_mapa = [
-    {"x": 80, "y": 80, "coletado": False},
-    {"x": 160, "y": 160, "coletado": False},
-    {"x": 200, "y": 0, "coletado": False},
-    {"x": 0, "y": 200, "coletado": False},
-]
