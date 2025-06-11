69b602716e8728cb9adcdc6761c47218b89f772d
Aline Pinho <alineveira2@gmail.com>
2025-06-10 23:52:01 -0300

    Create Guilda.jsx

---
+++ frontend/src/components/LojaGuilda/Guilda.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Guilda() {
+    return (
+        <div>
+            <h4>Guilda</h4>
+            <p>Troque seus recursos por moedas ou buffs aqui!</p>
+        </div>
+    );
+}
